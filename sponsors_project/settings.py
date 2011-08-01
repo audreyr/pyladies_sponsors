ba88499e98725ce74a11667efad2d0f3f12e6be5
Audrey M Roy <audreyr@gmail.com>
2011-08-01 14:42:39 -0700

    Added fixture generator and starter fixture. Modified detail page.

--- sponsors_project/settings.py
+++ sponsors_project/settings.py
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     'startcbv',
     'south',
     'sponsors',
+    'fixture_generator',
 )
 
 # A sample logging configuration. The only tangible logging
---
+++ sponsors_project/sponsors/fixture_gen.py
@@ -0,0 +1,11 @@
+from fixture_generator import fixture_generator
+
+from sponsors.models import Sponsor
+
+@fixture_generator(Sponsor)
+def test_sponsors():
+	cars = Sponsor.objects.create(name="Cars.com", slug="cars-dot-com", 
+	homepage_link="http://www.cars.com", 
+	careers_link="http:www.newcars.com/about/", 
+	description="<p>Cars.com provided food, drinks, and mentoring at the LA PyLadies' Django Day workshop on July 23, 2011.</p><p>Many of the Cars.com developers are also active participants in IRC channel #pyladies and at various LA PyLadies events.</p>", 
+	category="IK")
